cf91818f5d0b6516f8d97d34d62de2efa2eb0bc3
Canello Manuel <canellomanuel2@gmail.com>
2025-04-28 17:23:46 -0300

    cambios menores

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Game g1 = new Game();
-        g1.iniciarJuego();
+        new Game().iniciarJuego();
     }
 }
